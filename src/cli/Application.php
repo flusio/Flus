4ddba8cc30c4b871579a7e700722d523850c1d3e
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-06 12:27:32 +0200

    Setup database and migrations

---
+++ src/cli/Application.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace flusio\cli;
+
+/**
+ * This is the central class for the CLI. It is called from the cli file.
+ *
+ * @author Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class Application
+{
+    /** @var \Minz\Engine **/
+    private $engine;
+
+    /**
+     * Setup a Router and declare its routes.
+     */
+    public function __construct()
+    {
+        // Initialize the routes
+        $router = new \Minz\Router();
+        $router->addRoute('cli', '/system/setup', 'cli/System#setup');
+        $router->addRoute('cli', '/database/status', 'cli/Database#status');
+
+        $this->engine = new \Minz\Engine($router);
+        \Minz\Url::setRouter($router);
+    }
+
+    /**
+     * Execute a request.
+     *
+     * @param \Minz\Request $request
+     *
+     * @return \Minz\Response
+     */
+    public function run($request)
+    {
+        return $this->engine->run($request);
+    }
+}
