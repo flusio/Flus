22dbe30690641bf9fa0a20f65356e8da4c603d72
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-08 16:34:42 +0200

    Setup Turbolinks

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,6 +1,9 @@
+import Turbolinks from 'turbolinks';
 import { Application } from 'stimulus';
 
 import HelloController from './controllers/hello_controller.js';
+
+Turbolinks.start();
 
 const application = Application.start();
 application.register('hello', HelloController);
--- src/assets/javascripts/controllers/hello_controller.js
+++ src/assets/javascripts/controllers/hello_controller.js
@@ -2,6 +2,9 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     connect () {
+        if (document.documentElement.hasAttribute('data-turbolinks-preview')) {
+            return;
+        }
         console.log('Hello, Stimulus!', this.element);
     }
 };
