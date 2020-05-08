6aa6369282fa3682016e85ca2f1f520080e2f6c6
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-08 15:54:36 +0200

    Setup Stimulus

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+import { Application } from 'stimulus';
+
+import HelloController from './controllers/hello_controller.js';
+
+const application = Application.start();
+application.register('hello', HelloController);
