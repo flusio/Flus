91079fd2ecf2dbee38cbddd5171aba6d36264e74
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-10 16:36:38 +0200

    tec: Add a useful utils\Belt class

---
+++ src/utils/Belt.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace flusio\utils;
+
+/**
+ * The Belt is a collection of useful snippets to reuse within the application.
+ *
+ * @author Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class Belt
+{
+    /**
+     * Return if a string starts with a substring
+     *
+     * @see https://stackoverflow.com/a/834355
+     *
+     * @param string $haystack The string to look into
+     * @param string $needle The substring to look for
+     *
+     * @return boolean True if $haystack starts with $needle
+     */
+    public static function startsWith($haystack, $needle)
+    {
+         $needle_length = strlen($needle);
+         return substr($haystack, 0, $needle_length) === $needle;
+    }
+
+    /**
+     * Return if a string ends with a substring
+     *
+     * @see https://stackoverflow.com/a/834355
+     *
+     * @param string $haystack The string to look into
+     * @param string $needle The substring to look for
+     *
+     * @return boolean True if $haystack ends with $needle
+     */
+    public static function endsWith($haystack, $needle)
+    {
+        $needle_length = strlen($needle);
+        return substr($haystack, -$needle_length, $needle_length) === $needle;
+    }
+}
---
+++ tests/utils/BeltTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace flusio\utils;
+
+use PHPUnit\Framework\TestCase;
+
+class BeltTest extends TestCase
+{
+    public function testStartsWith()
+    {
+        $string = 'foobar';
+        $substring = 'foo';
+
+        $result = Belt::startsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testStartsWithWhenSubstringIsEmpty()
+    {
+        $string = 'foobar';
+        $substring = '';
+
+        $result = Belt::startsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testStartsWithWhenBothAreEmpty()
+    {
+        $string = '';
+        $substring = '';
+
+        $result = Belt::startsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testStartsWithWhenItDoesntStartsWithSubstring()
+    {
+        $string = 'foobar';
+        $substring = 'spam';
+
+        $result = Belt::startsWith($string, $substring);
+
+        $this->assertFalse($result);
+    }
+
+    public function testStartsWithWhenSubstringIsLonger()
+    {
+        $string = 'foobar';
+        $substring = 'foobarspam';
+
+        $result = Belt::startsWith($string, $substring);
+
+        $this->assertFalse($result);
+    }
+
+    public function testEndsWith()
+    {
+        $string = 'foobar';
+        $substring = 'bar';
+
+        $result = Belt::endsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testEndsWithWhenSubstringIsEmpty()
+    {
+        $string = 'foobar';
+        $substring = '';
+
+        $result = Belt::endsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testEndsWithWhenBothAreEmpty()
+    {
+        $string = '';
+        $substring = '';
+
+        $result = Belt::endsWith($string, $substring);
+
+        $this->assertTrue($result);
+    }
+
+    public function testEndsWithWhenItDoesntEndsWithSubstring()
+    {
+        $string = 'foobar';
+        $substring = 'spam';
+
+        $result = Belt::endsWith($string, $substring);
+
+        $this->assertFalse($result);
+    }
+
+    public function testEndsWithWhenSubstringIsLonger()
+    {
+        $string = 'foobar';
+        $substring = 'spamfoobar';
+
+        $result = Belt::endsWith($string, $substring);
+
+        $this->assertFalse($result);
+    }
+}
