c992eca9ef6a8ccf3350b058b44c56210b3f0104
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-12 08:30:15 +0200

    tec: Add Faker to be used in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,3 +7,15 @@ include $app_path . '/autoload.php';
 \Minz\Configuration::load('test', $app_path);
 \Minz\Environment::initialize();
 \Minz\Environment::startSession();
+
+$faker = \Faker\Factory::create();
+
+$faker_seed = getenv('SEED');
+if ($faker_seed) {
+    $faker_seed = intval($faker_seed);
+} else {
+    $faker_seed = random_int(PHP_INT_MIN, PHP_INT_MAX);
+}
+
+$faker->seed($faker_seed);
+echo 'Use SEED=' . $faker_seed . " to reproduce this suite.\n";
