flusio/Flus

commit 8fdf11b9fca04b0d95355e3de346be9b0de85fec

Change relation between users and tokens
A token doesn't need to know about a user. The token type can be
determined by the relation from the user. It means tokens are way more
generice than before.

src/Users.php
@@ -87,10 +87,12 @@ class Users
             ]);
         }
 
+        $token = models\Token::init();
+        $token_dao->save($token);
+
+        $user->setProperty('validation_token', $token->token);
         $user_dao->save($user);
 
-        $token = models\Token::initRegistrationValidationToken($user->id);
-        $token_dao->save($token);
         $users_mailer = new mailers\Users();
         $users_mailer->sendRegistrationValidationEmail($user, $token);
 
@@ -130,7 +132,7 @@ class Users
             ]);
         }
 
-        $raw_user = $user_dao->find($token->user_id);
+        $raw_user = $user_dao->findBy(['validation_token' => $token->token]);
         $user = new models\User($raw_user);
         if ($user->validated_at) {
             return Response::redirect('home');

tests/UsersTest.php
@@ -58,8 +58,7 @@ class UsersTest extends Tests\IntegrationTestCase
 
         $user = new Models\User($user_dao->listAll()[0]);
         $token = new Models\Token($token_dao->listAll()[0]);
-        $this->assertSame($user->id, $token->user_id);
-        $this->assertSame('registration_validation', $token->type);
+        $this->assertSame($user->validation_token, $token->token);
         $this->assertEquals(\Minz\Time::fromNow(1, 'day'), $token->expired_at);
 
         \Minz\Time::unfreeze();
@@ -219,13 +218,13 @@ class UsersTest extends Tests\IntegrationTestCase
         $user_dao = new models\dao\User();
         \Minz\Time::freeze($faker->dateTime());
 
+        $expired_at = \Minz\Time::fromNow($faker->numberBetween(1, 9000), 'minutes');
+        $token = self::$factories['tokens']->create([
+            'expired_at' => $expired_at->format(\Minz\Model::DATETIME_FORMAT),
+        ]);
         $user_id = self::$factories['users']->create([
             'validated_at' => null,
-        ]);
-        $token = self::$factories['tokens']->create([
-            'type' => 'registration_validation',
-            'expired_at' => \Minz\Time::fromNow($faker->randomNumber, 'minutes')->format(\Minz\Model::DATETIME_FORMAT),
-            'user_id' => $user_id,
+            'validation_token' => $token,
         ]);
 
         $request = new \Minz\Request('get', '/registration/validation', [
@@ -245,13 +244,13 @@ class UsersTest extends Tests\IntegrationTestCase
         $user_dao = new models\dao\User();
         \Minz\Time::freeze($faker->dateTime());
 
+        $expired_at = \Minz\Time::fromNow($faker->numberBetween(1, 9000), 'minutes');
+        $token = self::$factories['tokens']->create([
+            'expired_at' => $expired_at->format(\Minz\Model::DATETIME_FORMAT),
+        ]);
         $user_id = self::$factories['users']->create([
             'validated_at' => $faker->iso8601,
-        ]);
-        $token = self::$factories['tokens']->create([
-            'type' => 'registration_validation',
-            'expired_at' => \Minz\Time::fromNow($faker->randomNumber, 'minutes')->format(\Minz\Model::DATETIME_FORMAT),
-            'user_id' => $user_id,
+            'validation_token' => $token,
         ]);
 
         $request = new \Minz\Request('get', '/registration/validation', [
@@ -271,13 +270,13 @@ class UsersTest extends Tests\IntegrationTestCase
         $user_dao = new models\dao\User();
         \Minz\Time::freeze($faker->dateTime());
 
+        $expired_at = \Minz\Time::ago($faker->numberBetween(1, 9000), 'minutes');
+        $token = self::$factories['tokens']->create([
+            'expired_at' => $expired_at->format(\Minz\Model::DATETIME_FORMAT),
+        ]);
         $user_id = self::$factories['users']->create([
             'validated_at' => null,
-        ]);
-        $token = self::$factories['tokens']->create([
-            'type' => 'registration_validation',
-            'expired_at' => \Minz\Time::ago($faker->randomNumber, 'minutes')->format(\Minz\Model::DATETIME_FORMAT),
-            'user_id' => $user_id,
+            'validation_token' => $token,
         ]);
 
         $request = new \Minz\Request('get', '/registration/validation', [
@@ -297,13 +296,13 @@ class UsersTest extends Tests\IntegrationTestCase
         $user_dao = new models\dao\User();
         \Minz\Time::freeze($faker->dateTime());
 
+        $expired_at = \Minz\Time::fromNow($faker->numberBetween(1, 9000), 'minutes');
+        $token = self::$factories['tokens']->create([
+            'expired_at' => $expired_at->format(\Minz\Model::DATETIME_FORMAT),
+        ]);
         $user_id = self::$factories['users']->create([
             'validated_at' => null,
-        ]);
-        $token = self::$factories['tokens']->create([
-            'type' => 'registration_validation',
-            'expired_at' => \Minz\Time::ago($faker->randomNumber, 'minutes')->format(\Minz\Model::DATETIME_FORMAT),
-            'user_id' => $user_id,
+            'validation_token' => $token,
         ]);
 
         $request = new \Minz\Request('get', '/registration/validation', [

src/migrations/Migration202005111150CreateToken.php
@@ -5,7 +5,7 @@ namespace flusio\migrations;
 /**
  * @codeCoverageIgnore
  */
-class Migration202005111350CreateToken
+class Migration202005111150CreateToken
 {
     public function migrate()
     {
@@ -16,8 +16,6 @@ class Migration202005111350CreateToken
                 token TEXT PRIMARY KEY,
                 created_at TIMESTAMPTZ NOT NULL,
                 expired_at TIMESTAMPTZ NOT NULL,
-                type TEXT NOT NULL,
-                user_id TEXT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
                 invalidated_at TIMESTAMPTZ
             );
         SQL;

src/migrations/Migration202005111330CreateUser.php
@@ -5,7 +5,7 @@ namespace flusio\migrations;
 /**
  * @codeCoverageIgnore
  */
-class Migration202005111130CreateUser
+class Migration202005111330CreateUser
 {
     public function migrate()
     {
@@ -18,7 +18,8 @@ class Migration202005111130CreateUser
                 email TEXT UNIQUE NOT NULL,
                 username TEXT NOT NULL,
                 password_hash TEXT NOT NULL,
-                validated_at TIMESTAMPTZ
+                validated_at TIMESTAMPTZ,
+                validation_token TEXT REFERENCES tokens ON DELETE SET NULL ON UPDATE CASCADE
             );
         SQL;
 

src/models/Token.php
@@ -8,8 +8,6 @@ namespace flusio\models;
  */
 class Token extends \Minz\Model
 {
-    public const VALID_TYPES = ['registration_validation'];
-
     public const PROPERTIES = [
         'created_at' => 'datetime',
 
@@ -25,31 +23,16 @@ class Token extends \Minz\Model
             'required' => true,
             'validator' => '\flusio\models\Token::validateToken',
         ],
-
-        'type' => [
-            'type' => 'string',
-            'required' => true,
-            'validator' => '\flusio\models\Token::validateType',
-        ],
-
-        'user_id' => [
-            'type' => 'string',
-            'required' => true,
-        ],
     ];
 
     /**
-     * @param string $user_id
-     *
-     * @throws \Minz\Error\ModelPropertyError if one of the property is invalid
+     * Initialize a token
      */
-    public static function initRegistrationValidationToken($user_id)
+    public static function init()
     {
         return new self([
             'expired_at' => \Minz\Time::fromNow(1, 'day'),
             'token' => bin2hex(random_bytes(8)),
-            'type' => 'registration_validation',
-            'user_id' => $user_id,
         ]);
     }
 
@@ -83,14 +66,5 @@ class Token extends \Minz\Model
     public static function validateToken($token)
     {
         return strlen($token) >= 16;
-    }
-
-    /**
-     * @param string $type
-     * @return boolean
-     */
-    public static function validateType($type)
-    {
-        return in_array($type, self::VALID_TYPES);
     }
 }

src/models/User.php
@@ -20,6 +20,8 @@ class User extends \Minz\Model
 
         'validated_at' => 'datetime',
 
+        'validation_token' => 'string',
+
         'email' => [
             'type' => 'string',
             'required' => true,

src/schema.sql
@@ -1,17 +1,16 @@
+CREATE TABLE tokens (
+    token TEXT PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
+    expired_at TIMESTAMPTZ NOT NULL,
+    invalidated_at TIMESTAMPTZ
+);
+
 CREATE TABLE users (
     id TEXT PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
     email TEXT UNIQUE NOT NULL,
     username TEXT NOT NULL,
     password_hash TEXT NOT NULL,
-    validated_at TIMESTAMPTZ
-);
-
-CREATE TABLE tokens (
-    token TEXT PRIMARY KEY,
-    created_at TIMESTAMPTZ NOT NULL,
-    expired_at TIMESTAMPTZ NOT NULL,
-    type TEXT NOT NULL,
-    user_id TEXT NOT NULL REFERENCES users ON DELETE CASCADE ON UPDATE CASCADE,
-    invalidated_at TIMESTAMPTZ
+    validated_at TIMESTAMPTZ,
+    validation_token TEXT REFERENCES tokens ON DELETE SET NULL ON UPDATE CASCADE
 );

tests/bootstrap.php
@@ -56,12 +56,5 @@ echo 'Use SEED=' . $faker_seed . " to reproduce this suite.\n";
         'expired_at' => function () use ($faker) {
             return $faker->iso8601;
         },
-        'type' => function () use ($faker) {
-            return $faker->randomElement(\flusio\models\Token::VALID_TYPES);
-        },
-        'user_id' => function () {
-            $users_factory = new \Minz\Tests\DatabaseFactory('users');
-            return $users_factory->create();
-        },
     ]
 );

commit 57eab3e5272f60a11e576a5a70c7083f7ecc2043

fix: Return 404 if token isn't associated to User
This is a case I didn't imagine initially because the models were
associated differently. The bug was introduced in 8fdf11b when I changed
their relation.

src/Users.php
@@ -116,7 +116,7 @@ class Users
      * @response 200 if the token is valid and the registration validated
      * @response 302 / if the token is valid and the registration already validated
      * @response 400 if the token has expired
-     * @response 404 if the token doesn't exist
+     * @response 404 if the token doesn't exist or not associated to a User
      *
      * @param \Minz\Request $request
      *
@@ -142,6 +142,12 @@ class Users
         }
 
         $raw_user = $user_dao->findBy(['validation_token' => $token->token]);
+        if (!$raw_user) {
+            return Response::notFound('users/validation.phtml', [
+                'error' => _('The token doesn’t exist.'),
+            ]);
+        }
+
         $user = new models\User($raw_user);
         if ($user->validated_at) {
             return Response::redirect('home');

tests/UsersTest.php
@@ -320,6 +320,21 @@ class UsersTest extends \PHPUnit\Framework\TestCase
         $this->assertNull($user->validated_at);
     }
 
+    public function testValidationFailsIfTokenIsNotAssociatedToAUser()
+    {
+        $faker = \Faker\Factory::create();
+        $expired_at = \Minz\Time::fromNow($faker->numberBetween(1, 9000), 'minutes');
+        $token = $this->create('token', [
+            'expired_at' => $expired_at->format(\Minz\Model::DATETIME_FORMAT),
+        ]);
+
+        $response = $this->appRun('get', '/registration/validation', [
+            't' => $token,
+        ]);
+
+        $this->assertResponse($response, 404, 'The token doesn’t exist');
+    }
+
     public function testValidationFailsIfTokenDoesNotExist()
     {
         $faker = \Faker\Factory::create();