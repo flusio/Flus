ec763b0e3f4898c44c5dea5eb83f9c796ced0ff0
Marien Fressinaud <dev@marienfressinaud.fr>
2020-05-18 16:14:10 +0200

    tec: Test localeToBCP47 function

---
+++ tests/utils/ViewHelpersTest.php
@@ -0,0 +1,32 @@
+<?php
+
+// phpcs:disable PSR1.Classes.ClassDeclaration.MissingNamespace
+class ViewHelpersTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @beforeClass
+     */
+    public static function loadViewHelpers()
+    {
+        // This is done in the src/Application.php file normally
+        include_once(\Minz\Configuration::$app_path . '/src/utils/view_helpers.php');
+    }
+
+    public function testLocaleToBcp47TransformsLocale()
+    {
+        $locale = 'en_GB';
+
+        $bcp47 = localeToBCP47($locale);
+
+        $this->assertSame('en-GB', $bcp47);
+    }
+
+    public function testLocaleToBcp47TransformsLocaleIfNoUnderscore()
+    {
+        $locale = 'en';
+
+        $bcp47 = localeToBCP47($locale);
+
+        $this->assertSame('en', $bcp47);
+    }
+}
