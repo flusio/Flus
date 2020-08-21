97b8fed71a5c4d12bd99d400bf724947d221180d
Marien Fressinaud <dev@marienfressinaud.fr>
2020-08-21 17:11:14 +0200

    tec: Return l10n key if value doesn't exist (JS)

--- src/assets/javascripts/l10n.js
+++ src/assets/javascripts/l10n.js
@@ -1,3 +1,8 @@
 export default function (l10nKey) {
-    return window.jsConfiguration.l10n[l10nKey];
+    const l10nValue = window.jsConfiguration.l10n[l10nKey];
+    if (l10nValue) {
+        return l10nValue;
+    } else {
+        return l10nKey;
+    }
 };
