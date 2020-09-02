d748fb5422cd8653d51850a5be362f8c92c882e2
Marien Fressinaud <dev@marienfressinaud.fr>
2020-09-02 08:50:58 +0200

    fix: Fix autofocus on link collections edition
    The modal was trying to set the focus on the hidden multiple select,
    which failed. For some unknown reason, Firefox was giving it back to the
    close button, which is not what we want.

--- src/assets/javascripts/controllers/modal_controller.js
+++ src/assets/javascripts/controllers/modal_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from 'stimulus';
 
 const FOCUSABLE_ELEMENTS = [
-    'a[href]',
-    'button:not([disabled])',
-    'input:not([disabled]):not([type="hidden"])',
-    'select:not([disabled])',
-    'textarea:not([disabled])',
+    'a[href]:not([tabindex="-1"])',
+    'button:not([disabled]):not([tabindex="-1"])',
+    'input:not([disabled]):not([type="hidden"]):not([tabindex="-1"])',
+    'select:not([disabled]):not([tabindex="-1"])',
+    'textarea:not([disabled]):not([tabindex="-1"])',
     '[tabindex]:not([tabindex="-1"])',
 ];
 
