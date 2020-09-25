10110aa3f4b948ae1d1b646c7591bd3d362504d9
Marien Fressinaud <dev@marienfressinaud.fr>
2020-09-25 17:36:28 +0200

    Add a page to configure news preferences

--- tests/NewsLinksTest.php
+++ tests/NewsLinksTest.php
@@ -156,6 +156,232 @@ class NewsLinksTest extends \PHPUnit\Framework\TestCase
         $this->assertResponse($response, 302, '/login?redirect_to=%2Fnews');
     }
 
+    public function testPreferencesRendersCorrectly()
+    {
+        $preferences = models\NewsPreferences::init(666, true, true, true);
+        $user = $this->login([
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('get', '/news/preferences');
+
+        $this->assertResponse($response, 200, 666);
+        $this->assertPointer($response, 'news_links/preferences.phtml');
+    }
+
+    public function testPreferencesRedirectsIfNotConnected()
+    {
+        $preferences = models\NewsPreferences::init(666, true, true, true);
+        $user_id = $this->create('user', [
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('get', '/news/preferences');
+
+        $this->assertResponse($response, 302, '/login?redirect_to=%2Fnews%2Fpreferences');
+    }
+
+    public function testUpdatePreferencesSavesUserNewsPreferencesAndRedirects()
+    {
+        $user_dao = new models\dao\User();
+        $min_duration = models\NewsPreferences::MIN_DURATION;
+        $max_duration = models\NewsPreferences::MAX_DURATION;
+        $old_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $new_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $old_from_bookmarks = $this->fake('boolean');
+        $new_from_bookmarks = true;
+        $old_from_followed = $this->fake('boolean');
+        $new_from_followed = true;
+        $old_from_topics = $this->fake('boolean');
+        $new_from_topics = true;
+        $preferences = models\NewsPreferences::init(
+            $old_duration,
+            $old_from_bookmarks,
+            $old_from_followed,
+            $old_from_topics
+        );
+        $user = $this->login([
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('post', '/news/preferences', [
+            'csrf' => $user->csrf,
+            'duration' => $new_duration,
+            'from_bookmarks' => $new_from_bookmarks,
+            'from_followed' => $new_from_followed,
+            'from_topics' => $new_from_topics,
+        ]);
+
+        $this->assertResponse($response, 302, '/news');
+        $user = new models\User($user_dao->find($user->id));
+        $news_preferences = models\NewsPreferences::fromJson($user->news_preferences);
+        $this->assertSame($new_duration, $news_preferences->duration);
+        $this->assertSame($new_from_bookmarks, $news_preferences->from_bookmarks);
+        $this->assertSame($new_from_followed, $news_preferences->from_followed);
+        $this->assertSame($new_from_topics, $news_preferences->from_topics);
+    }
+
+    public function testUpdatePreferencesRedirectsIfNotConnected()
+    {
+        $user_dao = new models\dao\User();
+        $min_duration = models\NewsPreferences::MIN_DURATION;
+        $max_duration = models\NewsPreferences::MAX_DURATION;
+        $old_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $new_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $old_from_bookmarks = $this->fake('boolean');
+        $new_from_bookmarks = true;
+        $old_from_followed = $this->fake('boolean');
+        $new_from_followed = true;
+        $old_from_topics = $this->fake('boolean');
+        $new_from_topics = true;
+        $preferences = models\NewsPreferences::init(
+            $old_duration,
+            $old_from_bookmarks,
+            $old_from_followed,
+            $old_from_topics
+        );
+        $user_id = $this->create('user', [
+            'csrf' => 'a token',
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('post', '/news/preferences', [
+            'csrf' => 'a token',
+            'duration' => $new_duration,
+            'from_bookmarks' => $new_from_bookmarks,
+            'from_followed' => $new_from_followed,
+            'from_topics' => $new_from_topics,
+        ]);
+
+        $this->assertResponse($response, 302, '/login?redirect_to=%2Fnews%2Fpreferences');
+        $user = new models\User($user_dao->find($user_id));
+        $news_preferences = models\NewsPreferences::fromJson($user->news_preferences);
+        $this->assertSame($old_duration, $news_preferences->duration);
+        $this->assertSame($old_from_bookmarks, $news_preferences->from_bookmarks);
+        $this->assertSame($old_from_followed, $news_preferences->from_followed);
+        $this->assertSame($old_from_topics, $news_preferences->from_topics);
+    }
+
+    public function testUpdatePreferencesFailsIfCsrfIsInvalid()
+    {
+        $user_dao = new models\dao\User();
+        $min_duration = models\NewsPreferences::MIN_DURATION;
+        $max_duration = models\NewsPreferences::MAX_DURATION;
+        $old_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $new_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $old_from_bookmarks = $this->fake('boolean');
+        $new_from_bookmarks = true;
+        $old_from_followed = $this->fake('boolean');
+        $new_from_followed = true;
+        $old_from_topics = $this->fake('boolean');
+        $new_from_topics = true;
+        $preferences = models\NewsPreferences::init(
+            $old_duration,
+            $old_from_bookmarks,
+            $old_from_followed,
+            $old_from_topics
+        );
+        $user = $this->login([
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('post', '/news/preferences', [
+            'csrf' => 'not the token',
+            'duration' => $new_duration,
+            'from_bookmarks' => $new_from_bookmarks,
+            'from_followed' => $new_from_followed,
+            'from_topics' => $new_from_topics,
+        ]);
+
+        $this->assertResponse($response, 400, 'A security verification failed');
+        $user = new models\User($user_dao->find($user->id));
+        $news_preferences = models\NewsPreferences::fromJson($user->news_preferences);
+        $this->assertSame($old_duration, $news_preferences->duration);
+        $this->assertSame($old_from_bookmarks, $news_preferences->from_bookmarks);
+        $this->assertSame($old_from_followed, $news_preferences->from_followed);
+        $this->assertSame($old_from_topics, $news_preferences->from_topics);
+    }
+
+    public function testUpdatePreferencesFailsIfDurationIsInvalid()
+    {
+        $user_dao = new models\dao\User();
+        $min_duration = models\NewsPreferences::MIN_DURATION;
+        $max_duration = models\NewsPreferences::MAX_DURATION;
+        $old_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $new_duration = $this->fake('randomNumber') * -1;
+        $old_from_bookmarks = $this->fake('boolean');
+        $new_from_bookmarks = true;
+        $old_from_followed = $this->fake('boolean');
+        $new_from_followed = true;
+        $old_from_topics = $this->fake('boolean');
+        $new_from_topics = true;
+        $preferences = models\NewsPreferences::init(
+            $old_duration,
+            $old_from_bookmarks,
+            $old_from_followed,
+            $old_from_topics
+        );
+        $user = $this->login([
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('post', '/news/preferences', [
+            'csrf' => $user->csrf,
+            'duration' => $new_duration,
+            'from_bookmarks' => $new_from_bookmarks,
+            'from_followed' => $new_from_followed,
+            'from_topics' => $new_from_topics,
+        ]);
+
+        $this->assertResponse($response, 400, "The duration must be between {$min_duration} and {$max_duration}");
+        $user = new models\User($user_dao->find($user->id));
+        $news_preferences = models\NewsPreferences::fromJson($user->news_preferences);
+        $this->assertSame($old_duration, $news_preferences->duration);
+        $this->assertSame($old_from_bookmarks, $news_preferences->from_bookmarks);
+        $this->assertSame($old_from_followed, $news_preferences->from_followed);
+        $this->assertSame($old_from_topics, $news_preferences->from_topics);
+    }
+
+    public function testUpdatePreferencesFailsIfNoFromIsSelected()
+    {
+        $user_dao = new models\dao\User();
+        $min_duration = models\NewsPreferences::MIN_DURATION;
+        $max_duration = models\NewsPreferences::MAX_DURATION;
+        $old_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $new_duration = $this->fake('numberBetween', $min_duration, $max_duration);
+        $old_from_bookmarks = $this->fake('boolean');
+        $new_from_bookmarks = false;
+        $old_from_followed = $this->fake('boolean');
+        $new_from_followed = false;
+        $old_from_topics = $this->fake('boolean');
+        $new_from_topics = false;
+        $preferences = models\NewsPreferences::init(
+            $old_duration,
+            $old_from_bookmarks,
+            $old_from_followed,
+            $old_from_topics
+        );
+        $user = $this->login([
+            'news_preferences' => $preferences->toJson(),
+        ]);
+
+        $response = $this->appRun('post', '/news/preferences', [
+            'csrf' => $user->csrf,
+            'duration' => $new_duration,
+            'from_bookmarks' => $new_from_bookmarks,
+            'from_followed' => $new_from_followed,
+            'from_topics' => $new_from_topics,
+        ]);
+
+        $this->assertResponse($response, 400, 'You must select at least one option');
+        $user = new models\User($user_dao->find($user->id));
+        $news_preferences = models\NewsPreferences::fromJson($user->news_preferences);
+        $this->assertSame($old_duration, $news_preferences->duration);
+        $this->assertSame($old_from_bookmarks, $news_preferences->from_bookmarks);
+        $this->assertSame($old_from_followed, $news_preferences->from_followed);
+        $this->assertSame($old_from_topics, $news_preferences->from_topics);
+    }
+
     public function testFillSelectsLinksFromBookmarksAndRedirects()
     {
         $news_link_dao = new models\dao\NewsLink();
