1f741fc866c78abecce6bdb8efddf15568171531
Marien Fressinaud <dev@marienfressinaud.fr>
2020-10-20 11:27:36 +0200

    imp: Hide back anchors on public page if no back URL

--- src/assets/javascripts/controllers/back_anchor_controller.js
+++ src/assets/javascripts/controllers/back_anchor_controller.js
@@ -4,9 +4,14 @@ export default class extends Controller {
     connect () {
         const type = this.data.get('type');
         const replace = this.data.get('replace');
+        const mode = this.data.get('mode');
         const backUrl = window.localStorage.getItem('back-' + type);
         if (replace && backUrl) {
             this.element.setAttribute(replace, backUrl);
+        }
+
+        if (!backUrl && mode === 'hide-if-none') {
+            this.element.remove();
         }
     }
 };
