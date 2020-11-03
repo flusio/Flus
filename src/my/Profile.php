dae7720e3d4bdd9f1cee359f987fab7435cf87ad
Marien Fressinaud <dev@marienfressinaud.fr>
2020-11-03 17:44:16 +0100

    Add an endpoint for the WebExtension

--- src/my/Profile.php
+++ src/my/Profile.php
@@ -132,4 +132,34 @@ class Profile
             'topic_ids' => $topic_ids,
         ]);
     }
+
+    /**
+     * Return some useful information for the browser extension
+     *
+     * @response 302 /login?redirect_to=/my/info.json
+     *    If the user is not connected
+     * @response 200
+     *    On success
+     */
+    public function info($request)
+    {
+        $user = utils\CurrentUser::get();
+        if (!$user) {
+            return Response::redirect('login', [
+                'redirect_to' => \Minz\Url::for('profile info'),
+            ]);
+        }
+
+        $bookmarks = $user->bookmarks();
+        $links = $bookmarks->links();
+        $json_output = json_encode([
+            'csrf' => $user->csrf,
+            'bookmarks_id' => $bookmarks->id,
+            'bookmarked_urls' => array_column($links, 'url'),
+        ]);
+
+        $response = Response::text(200, $json_output);
+        $response->setHeader('Content-Type', 'application/json');
+        return $response;
+    }
 }
--- tests/my/ProfileTest.php
+++ tests/my/ProfileTest.php
@@ -236,4 +236,44 @@ class ProfileTest extends \PHPUnit\Framework\TestCase
         $topic_ids = array_column($user->topics(), 'id');
         $this->assertSame([$old_topic_id], $topic_ids);
     }
+
+    public function testInfoRendersCorrectly()
+    {
+        $user = $this->login();
+        $url_1 = $this->fakeUnique('url');
+        $url_2 = $this->fakeUnique('url');
+        $bookmarks_id = $this->create('collection', [
+            'user_id' => $user->id,
+            'type' => 'bookmarks',
+        ]);
+        $link_id_1 = $this->create('link', [
+            'user_id' => $user->id,
+            'url' => $url_1,
+        ]);
+        $link_id_2 = $this->create('link', [
+            'user_id' => $user->id,
+            'url' => $url_2,
+        ]);
+        $this->create('link_to_collection', [
+            'collection_id' => $bookmarks_id,
+            'link_id' => $link_id_1,
+        ]);
+
+        $response = $this->appRun('get', '/my/info.json');
+
+        $this->assertResponse($response, 200, null, [
+            'Content-Type' => 'application/json',
+        ]);
+        $output = json_decode($response->render(), true);
+        $this->assertSame($user->csrf, $output['csrf']);
+        $this->assertSame($bookmarks_id, $output['bookmarks_id']);
+        $this->assertSame([$url_1], $output['bookmarked_urls']);
+    }
+
+    public function testInfoRedirectsIfUserNotConnected()
+    {
+        $response = $this->appRun('get', '/my/info.json');
+
+        $this->assertResponse($response, 302, '/login?redirect_to=%2Fmy%2Finfo.json');
+    }
 }
