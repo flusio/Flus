0ba0a3cccaf02a1d72563a2ca1e4c1c72c1f18f0
Marien Fressinaud <dev@marienfressinaud.fr>
2021-02-08 13:57:38 +0100

    tec: Fix xdebug config (coverage mode)

--- docker/Dockerfile.php
+++ docker/Dockerfile.php
@@ -17,7 +17,8 @@ RUN apt-get update && apt-get install -y \
     && docker-php-ext-configure intl \
     && docker-php-ext-configure gd --with-webp --with-jpeg --with-freetype \
     && docker-php-ext-install -j$(nproc) intl gettext zip pdo pdo_pgsql gd \
-    && docker-php-ext-enable xdebug
+    && docker-php-ext-enable xdebug \
+    && echo "xdebug.mode=coverage" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini;
 
 RUN echo 'en_GB.UTF-8 UTF-8' >> /etc/locale.gen && \
     echo 'fr_FR.UTF-8 UTF-8' >> /etc/locale.gen && \
