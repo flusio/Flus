4343ddf58b1c9a5b930deaa95789a1b435db7aa2
Marien Fressinaud <dev@marienfressinaud.fr>
2021-02-12 16:13:05 +0100

    tec: Allow to set timeout of autosubmit controller

--- src/assets/javascripts/controllers/autosubmit_controller.js
+++ src/assets/javascripts/controllers/autosubmit_controller.js
@@ -4,9 +4,15 @@ export default class extends Controller {
     connect () {
         const mode = this.data.get('mode');
         if (mode === 'timeout') {
+            let timeout;
+            if (this.data.has('timeout')) {
+                timeout = parseInt(this.data.get('timeout'), 10);
+            } else {
+                timeout = 500;
+            }
             window.setTimeout(() => {
                 this.submit();
-            }, 500);
+            }, timeout);
         }
     }
 
