b16c66601e25edb76f2061d98cbb5f4828802279
Marien Fressinaud <dev@marienfressinaud.fr>
2021-04-21 14:01:27 +0200

    imp: Redirect to search on "back" of feeds pages

--- src/assets/javascripts/controllers/back_storage_controller.js
+++ src/assets/javascripts/controllers/back_storage_controller.js
@@ -2,8 +2,10 @@ import { Controller } from 'stimulus';
 
 export default class extends Controller {
     initialize () {
-        const type = this.data.get('type');
+        const types = this.data.get('type').split(',');
         const currentPath = window.location.pathname + window.location.search;
-        window.localStorage.setItem('back-' + type, currentPath);
+        types.forEach((type) => {
+            window.localStorage.setItem('back-' + type.trim(), currentPath);
+        });
     }
 };
