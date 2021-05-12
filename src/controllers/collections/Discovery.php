05f23cabf1d3e104edd3f22fbfbae61990cb3c69
Marien Fressinaud <dev@marienfressinaud.fr>
2021-05-12 12:12:56 +0200

    Redirect old discovery page to the new one

--- src/controllers/collections/Discovery.php
+++ src/controllers/collections/Discovery.php
@@ -14,42 +14,12 @@ use flusio\utils;
 class Discovery
 {
     /**
-     * Show the discovery page
+     * Redirect to the discovery page
      *
-     * @request_param integer page
-     *
-     * @response 302 /login?redirect_to=/collections/discover if not connected
-     * @response 302 /collections/discover?page=:bounded_page if :page is invalid
-     * @response 200
+     * @response 302 /discovery
      */
     public function show($request)
     {
-        $user = auth\CurrentUser::get();
-        if (!$user) {
-            return Response::redirect('login', [
-                'redirect_to' => \Minz\Url::for('discover collections'),
-            ]);
-        }
-
-        $number_collections = models\Collection::daoCall('countForDiscovering', $user->id);
-
-        $pagination_page = intval($request->param('page', 1));
-        $pagination = new utils\Pagination($number_collections, 30, $pagination_page);
-        if ($pagination_page !== $pagination->currentPage()) {
-            return Response::redirect('discover collections', ['page' => $pagination->currentPage()]);
-        }
-
-        $collections = models\Collection::daoToList(
-            'listForDiscovering',
-            $user->id,
-            $pagination->currentOffset(),
-            $pagination->numberPerPage()
-        );
-        models\Collection::sort($collections, $user->locale);
-
-        return Response::ok('collections/discovery/show.phtml', [
-            'collections' => $collections,
-            'pagination' => $pagination,
-        ]);
+        return Response::redirect('discovery');
     }
 }
