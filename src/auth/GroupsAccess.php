51da11c4c1a4292062ab63700b9d9310cecd41ff
Marien Fressinaud <dev@marienfressinaud.fr>
2021-05-24 13:44:33 +0200

    Allow to update the groups names

---
+++ src/auth/GroupsAccess.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace flusio\auth;
+
+/**
+ * @author  Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class GroupsAccess
+{
+    public static function canUpdate($user, $group)
+    {
+        return $user && $group && $user->id === $group->user_id;
+    }
+
+    public static function canDelete($user, $group)
+    {
+        return $user && $group && $user->id === $group->user_id;
+    }
+}
