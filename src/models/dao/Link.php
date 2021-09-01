flusio/Flus

commit a70167cf4811df7564d6ab944e0ca9172b65e0b9

imp: Handle different link publication dates
A link can be added to a collection at a time "T", and added to another
collection at a time "T+1". Until now, the only date we saved was "T".
In consequence, it was possible for a freshly added link to be burried
at the end of a collection, just because it has been added in another
collection long time ago.

Handling different dates has several implications such as changing the
date we display, the links order column, how Pocket and feeds
publication dates are saved and how we handle publication dates in the
news. I didn't expect so many changes in this commit and I expect more
to come as long as I detect impacts… but it's a long waited change and I
should not postpone it more.

src/models/dao/Link.php
@@ -89,7 +89,7 @@ class Link extends \Minz\DatabaseModel
         }
 
         $sql = <<<SQL
-            SELECT l.*, (
+            SELECT l.*, lc.created_at AS published_at, (
                 SELECT COUNT(m.*) FROM messages m
                 WHERE m.link_id = l.id
             ) AS number_comments
@@ -100,7 +100,7 @@ class Link extends \Minz\DatabaseModel
 
             {$visibility_clause}
 
-            ORDER BY l.created_at DESC, l.id
+            ORDER BY lc.created_at DESC, l.id
             OFFSET :offset
             {$limit_clause}
         SQL;
@@ -152,7 +152,7 @@ class Link extends \Minz\DatabaseModel
     public function listForNews($user_id)
     {
         $sql = <<<SQL
-            SELECT l.*, nl.id AS news_link_id, (
+            SELECT l.*, nl.published_at AS published_at, nl.id AS news_link_id, (
                 SELECT COUNT(m.*) FROM messages m
                 WHERE m.link_id = l.id
             ) AS number_comments
@@ -165,7 +165,7 @@ class Link extends \Minz\DatabaseModel
             AND nl.read_at IS NULL
             AND (l.is_hidden = false OR l.user_id = :user_id)
 
-            ORDER BY l.created_at DESC, l.id
+            ORDER BY nl.published_at DESC, l.id
         SQL;
 
         $statement = $this->prepare($sql);
@@ -208,7 +208,7 @@ class Link extends \Minz\DatabaseModel
         }
 
         $sql = <<<SQL
-            SELECT l.*, 'bookmarks' AS news_via_type
+            SELECT l.*, lc.created_at AS published_at, 'bookmarks' AS news_via_type
             FROM links l, collections c, links_to_collections lc
 
             WHERE lc.link_id = l.id
@@ -221,7 +221,7 @@ class Link extends \Minz\DatabaseModel
 
             {$where_placeholder}
 
-            GROUP BY l.id
+            GROUP BY l.id, lc.created_at
 
             ORDER BY random()
         SQL;
@@ -266,7 +266,7 @@ class Link extends \Minz\DatabaseModel
         }
 
         $sql = <<<SQL
-            SELECT l.*, 'followed' AS news_via_type, c.id AS news_via_collection_id
+            SELECT l.*, lc.created_at AS published_at, 'followed' AS news_via_type, c.id AS news_via_collection_id
             FROM links l, collections c, links_to_collections lc, followed_collections fc
 
             WHERE fc.user_id = :user_id
@@ -295,9 +295,9 @@ class Link extends \Minz\DatabaseModel
 
             {$where_placeholder}
 
-            GROUP BY l.id, c.id
+            GROUP BY l.id, lc.created_at, c.id
 
-            ORDER BY l.created_at DESC, l.id
+            ORDER BY lc.created_at DESC, l.id
             LIMIT 30
         SQL;
 
@@ -375,7 +375,7 @@ class Link extends \Minz\DatabaseModel
             WHERE l.id = lc.link_id
             AND lc.collection_id = :collection_id
 
-            ORDER BY l.created_at DESC, l.id
+            ORDER BY lc.created_at DESC, l.id
             LIMIT 200
         SQL;
 

src/models/Link.php
@@ -77,6 +77,11 @@ class Link extends \Minz\Model
             'type' => 'string',
         ],
 
+        'published_at' => [
+            'type' => 'datetime',
+            'computed' => true,
+        ],
+
         'number_comments' => [
             'type' => 'integer',
             'computed' => true,

src/jobs/PocketImportator.php
@@ -204,17 +204,7 @@ class PocketImportator extends Job
                 // In normal cases, created_at is set on save() call. Since we
                 // add links via the bulkInsert call, we have to set created_at
                 // first, or it would fail because of the not-null constraint.
-                // If time_added is set (not documented), we use it to set the
-                // property in order to keep the order from Pocket. It defaults
-                // to "now".
-                $created_at = \Minz\Time::now();
-                if (isset($item['time_added'])) {
-                    $timestamp = intval($item['time_added']);
-                    if ($timestamp > 0) {
-                        $created_at->setTimestamp($timestamp);
-                    }
-                }
-                $link->created_at = $created_at;
+                $link->created_at = \Minz\Time::now();
 
                 // Then, add the link properties values to the list
                 $db_link = $link->toValues();
@@ -234,9 +224,21 @@ class PocketImportator extends Job
                 $link_id = $link->id;
             }
 
+            // If time_added is set (not documented), we use it to set the date
+            // of attachment to the collection in order to keep the order from
+            // Pocket. It defaults to now.
+            $published_at = \Minz\Time::now();
+            if (isset($item['time_added'])) {
+                $timestamp = intval($item['time_added']);
+                if ($timestamp > 0) {
+                    $published_at->setTimestamp($timestamp);
+                }
+            }
+
             // We now have a link_id and a list of collection ids. We store
             // here the relations in the last _to_create array.
             foreach ($collection_ids as $collection_id) {
+                $links_to_collections_to_create[] = $published_at->format(\Minz\Model::DATETIME_FORMAT);
                 $links_to_collections_to_create[] = $link_id;
                 $links_to_collections_to_create[] = $collection_id;
             }
@@ -262,7 +264,7 @@ class PocketImportator extends Job
         if ($links_to_collections_to_create) {
             $links_to_collections_dao = new models\dao\LinksToCollections();
             $links_to_collections_dao->bulkInsert(
-                ['link_id', 'collection_id'],
+                ['created_at', 'link_id', 'collection_id'],
                 $links_to_collections_to_create
             );
         }

src/migrations/Migration202108300003AddCreatedAtToLinksToCollections.php
@@ -1,0 +1,42 @@
+<?php
+
+namespace flusio\migrations;
+
+class Migration202108300003AddCreatedAtToLinksToCollections
+{
+    public function migrate()
+    {
+        $database = \Minz\Database::get();
+
+        $database->beginTransaction();
+
+        $database->exec(<<<'SQL'
+            ALTER TABLE links_to_collections
+            ADD COLUMN created_at TIMESTAMPTZ;
+
+            UPDATE links_to_collections lc
+            SET created_at = l.created_at
+            FROM links l
+            WHERE lc.link_id = l.id;
+
+            ALTER TABLE links_to_collections
+            ALTER COLUMN created_at SET NOT NULL;
+        SQL);
+
+        $database->commit();
+
+        return true;
+    }
+
+    public function rollback()
+    {
+        $database = \Minz\Database::get();
+
+        $database->exec(<<<'SQL'
+            ALTER TABLE links_to_collections
+            DROP COLUMN created_at;
+        SQL);
+
+        return true;
+    }
+}

src/migrations/Migration202108300004AddPublishedAtToNewsLinks.php
@@ -1,0 +1,39 @@
+<?php
+
+namespace flusio\migrations;
+
+class Migration202108300004AddPublishedAtToNewsLinks
+{
+    public function migrate()
+    {
+        $database = \Minz\Database::get();
+
+        $database->beginTransaction();
+
+        $database->exec(<<<'SQL'
+            ALTER TABLE news_links
+            ADD COLUMN published_at TIMESTAMPTZ;
+
+            UPDATE news_links nl
+            SET published_at = l.created_at
+            FROM links l
+            WHERE nl.link_id = l.id;
+        SQL);
+
+        $database->commit();
+
+        return true;
+    }
+
+    public function rollback()
+    {
+        $database = \Minz\Database::get();
+
+        $database->exec(<<<'SQL'
+            ALTER TABLE news_links
+            DROP COLUMN published_at;
+        SQL);
+
+        return true;
+    }
+}

src/models/NewsLink.php
@@ -19,6 +19,10 @@ class NewsLink extends \Minz\Model
             'type' => 'datetime',
         ],
 
+        'published_at' => [
+            'type' => 'datetime',
+        ],
+
         'url' => [
             'type' => 'string',
             'required' => true,
@@ -60,6 +64,7 @@ class NewsLink extends \Minz\Model
     public static function initFromLink($link, $user_id)
     {
         return new self([
+            'published_at' => $link->published_at,
             'url' => $link->url,
             'user_id' => $user_id,
             'link_id' => $link->id,

src/models/dao/LinksToCollections.php
@@ -17,7 +17,7 @@ class LinksToCollections extends \Minz\DatabaseModel
      */
     public function __construct()
     {
-        $properties = ['id', 'link_id', 'collection_id'];
+        $properties = ['id', 'created_at', 'link_id', 'collection_id'];
         parent::__construct('links_to_collections', 'id', $properties);
     }
 
@@ -31,16 +31,17 @@ class LinksToCollections extends \Minz\DatabaseModel
      */
     public function attach($link_id, $collection_ids)
     {
+        $now = \Minz\Time::now()->format(\Minz\Model::DATETIME_FORMAT);
         $values_as_question_marks = [];
         $values = [];
         foreach ($collection_ids as $collection_id) {
-            $values_as_question_marks[] = '(?, ?)';
-            $values = array_merge($values, [$link_id, $collection_id]);
+            $values_as_question_marks[] = '(?, ?, ?)';
+            $values = array_merge($values, [$now, $link_id, $collection_id]);
         }
         $values_placeholder = implode(", ", $values_as_question_marks);
 
         $sql = <<<SQL
-            INSERT INTO links_to_collections (link_id, collection_id)
+            INSERT INTO links_to_collections (created_at, link_id, collection_id)
             VALUES {$values_placeholder};
         SQL;
 

src/schema.sql
@@ -144,6 +144,7 @@ CREATE INDEX idx_links_fetched_code ON links(fetched_code);
 
 CREATE TABLE links_to_collections (
     id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL,
     link_id TEXT REFERENCES links ON DELETE CASCADE ON UPDATE CASCADE,
     collection_id TEXT REFERENCES collections ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -164,6 +165,7 @@ CREATE UNIQUE INDEX idx_followed_collections ON followed_collections(user_id, co
 CREATE TABLE news_links (
     id SERIAL PRIMARY KEY,
     created_at TIMESTAMPTZ NOT NULL,
+    published_at TIMESTAMPTZ,
     url TEXT NOT NULL,
     link_id TEXT REFERENCES links ON DELETE SET NULL ON UPDATE CASCADE,
     via_type TEXT NOT NULL DEFAULT '',

commit b745c6621b04db55ee2232f280465ad81d4d0242

fix: Fix until condition when filling news
The bug has been introduced by a70167c: the until condition must apply
on the publication date which is now stored in the links_to_collections
relation.

src/models/dao/Link.php
@@ -203,7 +203,7 @@ class Link extends \Minz\DatabaseModel
         }
 
         if ($until !== null) {
-            $where_placeholder .= 'AND l.created_at >= :until ';
+            $where_placeholder .= 'AND lc.created_at >= :until ';
             $values[':until'] = $until->format(\Minz\Model::DATETIME_FORMAT);
         }
 
@@ -261,7 +261,7 @@ class Link extends \Minz\DatabaseModel
         }
 
         if ($until !== null) {
-            $where_placeholder .= 'AND l.created_at >= :until ';
+            $where_placeholder .= 'AND lc.created_at >= :until ';
             $values[':until'] = $until->format(\Minz\Model::DATETIME_FORMAT);
         }
 

tests/services/NewsPickerTest.php
@@ -160,12 +160,10 @@ class NewsPickerTest extends \PHPUnit\Framework\TestCase
         ]);
         $link_id_1 = $this->create('link', [
             'user_id' => $this->other_user->id,
-            'created_at' => $created_at_1->format(\Minz\Model::DATETIME_FORMAT),
             'is_hidden' => 0,
         ]);
         $link_id_2 = $this->create('link', [
             'user_id' => $this->other_user->id,
-            'created_at' => $created_at_2->format(\Minz\Model::DATETIME_FORMAT),
             'is_hidden' => 0,
         ]);
         $collection_id = $this->create('collection', [
@@ -174,10 +172,12 @@ class NewsPickerTest extends \PHPUnit\Framework\TestCase
             'is_public' => 1,
         ]);
         $this->create('link_to_collection', [
+            'created_at' => $created_at_1->format(\Minz\Model::DATETIME_FORMAT),
             'collection_id' => $collection_id,
             'link_id' => $link_id_1,
         ]);
         $this->create('link_to_collection', [
+            'created_at' => $created_at_2->format(\Minz\Model::DATETIME_FORMAT),
             'collection_id' => $collection_id,
             'link_id' => $link_id_2,
         ]);