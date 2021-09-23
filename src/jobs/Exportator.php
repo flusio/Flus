27dfa7247ea9d235a5b9fa02c25b259092647ae5
Marien Fressinaud <dev@marienfressinaud.fr>
2021-09-23 12:33:43 +0200

    Provide an Exportator job

---
+++ src/jobs/Exportator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace flusio\jobs;
+
+use flusio\models;
+use flusio\services;
+
+/**
+ * Job to export data.
+ *
+ * @author  Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class Exportator extends Job
+{
+    /**
+     * @param integer $exportation_id
+     */
+    public function perform($exportation_id)
+    {
+        $exportation = models\Exportation::find($exportation_id);
+        if ($exportation->status !== 'ongoing') {
+            return;
+        }
+
+        $exportations_path = \Minz\Configuration::$data_path . '/exportations';
+        if (!file_exists($exportations_path)) {
+            @mkdir($exportations_path);
+        }
+
+        try {
+            $exportation_service = new services\DataExporter($exportations_path);
+            $filepath = $exportation_service->export($exportation->user_id);
+            $exportation->finish($filepath);
+        } catch (\Exception $e) {
+            $exportation->fail($e->getMessage());
+        }
+
+        $exportation->save();
+    }
+}
