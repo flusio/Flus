flusio/Flus

commit c41afda21953f47257d88be0551e3f62e2f3b7a2

fix: Fix the strategy to sanitize URLs
The previous strategy was generating bad URLs, for instance:

```
https://example.com/?url=http%3A%2F%2Fexample.fr%2F%3Ffoo%3Dbar&spam=egg
```

was sanitized to:

```
https://example.com/?url=http://example.fr/?foo=bar&spam=egg
```

instead of:

```
https://example.com/?url=http%3A%2F%2Fexample.fr%2F%3Ffoo%3Dbar&spam=egg
```

The issue is that in the first case, we weren't able anymore to tell if
the `spam` parameter was associated to the base URL or to the `url`
value.

Now, each part of the URL is encoded separately.

`query` is re-encoded by parsing it, encoding each parameter one by one
and rebuilding the query. This way, we are sure that the `spam`
parameter is processed separately from the `url` parameter.

This fix changes the result of some tests, but they look okay and normal
considering the expected behaviour of the example given here.

lib/SpiderBits/src/Url.php
@@ -101,19 +101,9 @@ class Url
             return '';
         }
 
-        // Then, we decode each part of the parsed URL. We want to decode as
-        // long as percent-encoding characters exist.
-        foreach ($parsed_url as $component => $value) {
-            while (preg_match('/%[0-9A-Fa-f]{2}/', $value) === 1) {
-                $value = rawurldecode($value);
-            }
-
-            $parsed_url[$component] = $value;
-        }
-
         // Get the scheme (default is http)
         if (isset($parsed_url['scheme'])) {
-            $scheme = $parsed_url['scheme'];
+            $scheme = self::fullPercentDecode($parsed_url['scheme']);
         } else {
             $scheme = 'http';
         }
@@ -128,6 +118,8 @@ class Url
         } else {
             $host = '';
         }
+
+        $host = self::fullPercentDecode($host);
 
         // Clean the extra dots from the host
         $host = trim($host, '.');
@@ -147,36 +139,31 @@ class Url
 
         // Get the path with ./ and ../ paths replaced.
         if (isset($parsed_url['path'])) {
-            $path = self::normalizePath($parsed_url['path']);
+            $path = self::fullPercentDecode($parsed_url['path']);
+            $path = self::normalizePath($path);
         } else {
             $path = '/';
         }
 
         // We finally rebuild the sanitized URL.
-        $sanitized_url = $scheme . '://';
-        $sanitized_url .= $host;
+        $sanitized_url = self::percentEncode($scheme) . '://';
+        $sanitized_url .= self::percentEncode($host);
         if (isset($parsed_url['port'])) {
-            $sanitized_url .= ':' . $parsed_url['port'];
+            $port = self::fullPercentDecode($parsed_url['port']);
+            $sanitized_url .= ':' . self::percentEncode($port);
         }
-        $sanitized_url .= $path;
+        $sanitized_url .= self::percentEncode($path);
         if (isset($parsed_url['query'])) {
-            $sanitized_url .= '?' . $parsed_url['query'];
+            $sanitized_url .= '?' . self::percentRecodeQuery($parsed_url['query']);
         } elseif (strpos($url, '?') !== false) {
             // If the initial URL had a `?` without query string, `parse_url()`
             // doesn't return the query component. We want to keep the question
             // mark though.
             $sanitized_url .= '?';
         }
-
-        // Re-percent-encode the URL. We don't want to use directly
-        // rawurlencode() since it will convert slashes (/), colons (:) and
-        // question mark (?)
-        $sanitized_url = self::percentEncode($sanitized_url);
-
-        // The fragment must be added afterwhile or the hash (#) could be
-        // converted.
         if (isset($parsed_url['fragment'])) {
-            $sanitized_url .= '#' . self::percentEncode($parsed_url['fragment']);
+            $fragment = self::fullPercentDecode($parsed_url['fragment']);
+            $sanitized_url .= '#' . self::percentEncode($fragment);
         }
 
         return $sanitized_url;
@@ -230,7 +217,26 @@ class Url
     }
 
     /**
-     * Percent-encode a URL.
+     * Percent-decode a value.
+     *
+     * It applies rawurldecode() on the value as long as a percent-encoded
+     * character is detected.
+     *
+     * @param string $value
+     *
+     * @return string
+     */
+    private static function fullPercentDecode($value)
+    {
+        while (preg_match('/%[0-9A-Fa-f]{2}/', $value) === 1) {
+            $value = rawurldecode($value);
+        }
+
+        return $value;
+    }
+
+    /**
+     * Percent-encode a value.
      *
      * Contrary to urlencode() and rawurlencode(), this method only encodes
      * ASCII characters <= 32, >= 127, '"', "#" and "%". This leaves for
@@ -239,14 +245,14 @@ class Url
      * @see https://www.php.net/manual/function.rawurlencode.php
      * @see https://en.wikipedia.org/wiki/ASCII
      *
-     * @param string url
+     * @param string $value
      *
      * @return string
      */
-    private static function percentEncode($url)
+    private static function percentEncode($value)
     {
         $escaped_url = '';
-        foreach (str_split($url) as $char) {
+        foreach (str_split($value) as $char) {
             $ord = ord($char);
             if ($ord > 32 && $ord < 127 && $char !== '"' && $char !== '#' && $char !== '%') {
                 $escaped_url .= $char;
@@ -255,5 +261,112 @@ class Url
             }
         }
         return $escaped_url;
+    }
+
+    /**
+     * Re-encode the query parameters.
+     *
+     * @param string $query
+     *
+     * @return string
+     */
+    private static function percentRecodeQuery($query)
+    {
+        $decoded_parameters = [];
+        $parameters = self::parseQuery($query);
+        foreach ($parameters as $name => $value) {
+            $name = rawurlencode(self::fullPercentDecode($name));
+            if ($value !== null) {
+                $value = rawurlencode(self::fullPercentDecode($value));
+            }
+            $decoded_parameters[$name] = $value;
+        }
+        return self::buildQuery($decoded_parameters);
+    }
+
+    /**
+     * Parse a query to parameters similarly to the parse_str function.
+     *
+     * Differences compared to parse_str:
+     *
+     * - the parameters are returned as an array;
+     * - the parameters with the same name are returned in an array instead of
+     *   being overwritten;
+     * - the value of parameters with no `=` sign is `null` instead of empty
+     *   string.
+     *
+     * @see https://www.php.net/manual/function.parse-str.php
+     *
+     * @param string $query
+     *
+     * @return array
+     */
+    public static function parseQuery($query)
+    {
+        if (empty($query)) {
+            return [];
+        }
+
+        $raw_parameters = explode('&', $query);
+        foreach ($raw_parameters as $raw_parameter) {
+            $exploded_parameter = explode('=', $raw_parameter, 2);
+            $name = $exploded_parameter[0];
+
+            if (count($exploded_parameter) === 2) {
+                $value = $exploded_parameter[1];
+            } else {
+                // The parameter may not contain a "=". Setting the value to
+                // null allows to distinguish both cases: `?foo` (value will
+                // be null) and `?foo=` (value will be empty string). This
+                // allows to rebuild the query properly later.
+                $value = null;
+            }
+
+            if (isset($parameters[$name])) {
+                if (!is_array($parameters[$name])) {
+                    $parameters[$name] = [$parameters[$name]];
+                }
+                $parameters[$name][] = $value;
+            } else {
+                $parameters[$name] = $value;
+            }
+        }
+
+        return $parameters;
+    }
+
+    /**
+     * Rebuild a query from parameters similarly to the http_build_query function.
+     *
+     * Differences compared to http_build_query:
+     *
+     * - parameters can only be an array;
+     * - it takes less arguments;
+     * - the arg separator is always "&";
+     * - it is expected than values are already encoded;
+     * - if a value is null, the parameter name is still appended, but without
+     *   an "=" sign.
+     *
+     * @see https://www.php.net/manual/function.http-build-query.php
+     *
+     * @param array $parameters
+     *
+     * @return string
+     */
+    public static function buildQuery($parameters)
+    {
+        $built_parameters = [];
+        foreach ($parameters as $name => $value) {
+            if ($value === null) {
+                $built_parameters[] = $name;
+            } elseif (is_array($value)) {
+                foreach ($value as $partial_value) {
+                    $built_parameters[] = "{$name}={$partial_value}";
+                }
+            } else {
+                $built_parameters[] = "{$name}={$value}";
+            }
+        }
+        return implode('&', $built_parameters);
     }
 }

tests/lib/SpiderBits/UrlTest.php
@@ -24,6 +24,26 @@ class UrlTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($expected, $sanitized_url);
     }
 
+    /**
+     * @dataProvider parseAndBuildQueryProvider
+     */
+    public function testParseQuery($query, $expected_parameters)
+    {
+        $parameters = Url::parseQuery($query);
+
+        $this->assertSame($expected_parameters, $parameters);
+    }
+
+    /**
+     * @dataProvider parseAndBuildQueryProvider
+     */
+    public function testBuildQuery($expected_query, $parameters)
+    {
+        $query = Url::buildQuery($parameters);
+
+        $this->assertSame($expected_query, $query);
+    }
+
     public function absolutizeProvider()
     {
         return [
@@ -75,12 +95,12 @@ class UrlTest extends \PHPUnit\Framework\TestCase
             ["http://www.google.com.../", 'http://www.google.com/'],
             ["http://www.google.com/foo\tbar\rbaz\n2",'http://www.google.com/foobarbaz2'],
             ["http://www.google.com/q?", 'http://www.google.com/q?'],
-            ["http://www.google.com/q?r?", 'http://www.google.com/q?r?'],
-            ["http://www.google.com/q?r?s", 'http://www.google.com/q?r?s'],
+            ["http://www.google.com/q?r?", 'http://www.google.com/q?r%3F'],
+            ["http://www.google.com/q?r?s", 'http://www.google.com/q?r%3Fs'],
             // We want to keep the fragment
             ["http://evil.com/foo#bar#baz", 'http://evil.com/foo#bar%23baz'],
             ["http://evil.com/foo;", 'http://evil.com/foo;'],
-            ["http://evil.com/foo?bar;", 'http://evil.com/foo?bar;'],
+            ["http://evil.com/foo?bar;", 'http://evil.com/foo?bar%3B'],
             // idn_to_ascii cannot handle this url (which is invalid anyway)
             // and return an empty host
             ["http://\x01\x80.com/", 'http:///'],
@@ -93,13 +113,16 @@ class UrlTest extends \PHPUnit\Framework\TestCase
             ["%20leadingspace.com/", 'http://%20leadingspace.com/'],
             ["https://www.securesite.com/", 'https://www.securesite.com/'],
             ["http://host.com/ab%23cd", 'http://host.com/ab%23cd'],
-            ["http://host.com//twoslashes?more//slashes", 'http://host.com/twoslashes?more//slashes'],
+            ["http://host.com//twoslashes?more//slashes", 'http://host.com/twoslashes?more%2F%2Fslashes'],
             // More tests
+            ['https://example.com/?url=http%3A%2F%2Fexample.fr%2F%3Ffoo%3Dbar&spam=egg', 'https://example.com/?url=http%3A%2F%2Fexample.fr%2F%3Ffoo%3Dbar&spam=egg'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ['https://example.com/?foo?=bar', 'https://example.com/?foo%3F=bar'],
+            ['https://example.com/?foo%26=bar&spam=egg', 'https://example.com/?foo%26=bar&spam=egg'],
             ["https://domén-with-accent.com?query=with-àccent", 'https://xn--domn-with-accent-dqb.com/?query=with-%C3%A0ccent'], // phpcs:ignore Generic.Files.LineLength.TooLong
             ["https://host.com?query=with-%C3%A0ccent", 'https://host.com/?query=with-%C3%A0ccent'],
-            ["https://host.com?utm_source=gazette%252B-%252Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
-            ["https://host.com?utm_source=gazette%2B-%2Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
-            ["https://host.com?utm_source=gazette+-+abonn%C3%A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette%252B-%252Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette%2B-%2Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette+-+abonn%C3%A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
             ["http://evil.com/foo#bar/baz", 'http://evil.com/foo#bar/baz'],
             ["http://evil.com/foo#bar%22baz", 'http://evil.com/foo#bar%22baz'],
             ["http://evil.com/foo#🐘", 'http://evil.com/foo#%F0%9F%90%98'],
@@ -107,6 +130,44 @@ class UrlTest extends \PHPUnit\Framework\TestCase
             ["http:///example.com", ''],
             ["http://:80", ''],
             ["http://user@:80", ''],
+        ];
+    }
+
+    public function parseAndBuildQueryProvider()
+    {
+        return [
+            [
+                'foo=bar',
+                ['foo' => 'bar'],
+            ],
+            [
+                'foo=bar&spam=egg',
+                ['foo' => 'bar', 'spam' => 'egg'],
+            ],
+            [
+                'id=1',
+                ['id' => '1'],
+            ],
+            [
+                'foo',
+                ['foo' => null],
+            ],
+            [
+                'foo=',
+                ['foo' => ''],
+            ],
+            [
+                'foo=bar&foo=baz',
+                ['foo' => ['bar', 'baz']],
+            ],
+            [
+                'foo[]=bar&foo[]=baz',
+                ['foo[]' => ['bar', 'baz']],
+            ],
+            [
+                '',
+                [],
+            ],
         ];
     }
 }

commit 49c324f802fe94e050c0ab1697892a4152758e8b

fix: Sanitize URL queries containing spaces correctly
Spaces are encoded as `+` (or `%20`) in URLs. Unfortunately I decoded
this character as a simple `+` instead of a space. When I re-encoded the
query value, the `+` was then encoded as `%2B`, which was wrong as it
should have stayed as `+` (or `%20`).

Now, I we re-encode queries, the character `+` is correctly decoded to a
space, and then re-encoded to `+`.

Bug introduced in: c41afda21953f47257d88be0551e3f62e2f3b7a2

lib/SpiderBits/src/Url.php
@@ -236,13 +236,20 @@ class Url
     /**
      * Percent-decode a value.
      *
-     * It applies rawurldecode() on the value as long as a percent-encoded
-     * character is detected.
+     * It applies rawurldecode() (or urldecode based on the raw parameter) on
+     * the value as long as a percent-encoded character is detected.
      */
-    private static function fullPercentDecode(string $value): string
+    private static function fullPercentDecode(string $value, bool $raw = true): string
     {
-        while (preg_match('/%[0-9A-Fa-f]{2}/', $value) === 1) {
-            $value = rawurldecode($value);
+        while (
+            (preg_match('/%[0-9A-Fa-f]{2}/', $value) === 1) ||
+            (!$raw && str_contains($value, '+'))
+        ) {
+            if ($raw) {
+                $value = rawurldecode($value);
+            } else {
+                $value = urldecode($value);
+            }
         }
 
         return $value;
@@ -280,17 +287,17 @@ class Url
         $decoded_parameters = [];
         $parameters = self::parseQuery($query);
         foreach ($parameters as $name => $value) {
-            $name = rawurlencode(self::fullPercentDecode($name));
+            $name = urlencode(self::fullPercentDecode($name, raw: false));
             if (is_array($value)) {
                 $value = array_map(function ($partial_value) {
                     if ($partial_value === null) {
                         return null;
                     } else {
-                        return rawurlencode(self::fullPercentDecode($partial_value));
+                        return urlencode(self::fullPercentDecode($partial_value, raw: false));
                     }
                 }, $value);
             } elseif ($value !== null) {
-                $value = rawurlencode(self::fullPercentDecode($value));
+                $value = urlencode(self::fullPercentDecode($value, raw: false));
             }
             $decoded_parameters[$name] = $value;
         }

tests/lib/SpiderBits/UrlTest.php
@@ -187,9 +187,9 @@ class UrlTest extends \PHPUnit\Framework\TestCase
             ['https://example.com/?foo=bar?&foo=baz?', 'https://example.com/?foo=bar%3F&foo=baz%3F'],
             ["https://domén-with-accent.com?query=with-àccent", 'https://xn--domn-with-accent-dqb.com/?query=with-%C3%A0ccent'], // phpcs:ignore Generic.Files.LineLength.TooLong
             ["https://host.com?query=with-%C3%A0ccent", 'https://host.com/?query=with-%C3%A0ccent'],
-            ["https://host.com?utm_source=gazette%252B-%252Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
-            ["https://host.com?utm_source=gazette%2B-%2Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
-            ["https://host.com?utm_source=gazette+-+abonn%C3%A9s", 'https://host.com/?utm_source=gazette%2B-%2Babonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette%252B-%252Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette%2B-%2Babonn%25C3%25A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
+            ["https://host.com?utm_source=gazette+-+abonn%C3%A9s", 'https://host.com/?utm_source=gazette+-+abonn%C3%A9s'], // phpcs:ignore Generic.Files.LineLength.TooLong
             ["http://evil.com/foo#bar/baz", 'http://evil.com/foo#bar/baz'],
             ["http://evil.com/foo#bar%22baz", 'http://evil.com/foo#bar%22baz'],
             ["http://evil.com/foo#🐘", 'http://evil.com/foo#%F0%9F%90%98'],