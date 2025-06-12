c73627e0ede7c222bd5bcfe4866ce13271c710ff
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-06-12 15:23:34 +0200

    new(api): Add an endpoint to mark a link to read later

--- docs/api.md
+++ docs/api.md
@@ -190,3 +190,44 @@ $ curl -H "Content-Type: application/json" \
        -X DELETE \
        https://app.flus.fr/api/v1/links/<id>/read
 ```
+
+## Mark a link to read later
+
+Mark a link to read later for the authenticated user:
+
+```http
+POST /api/v1/links/:id/later
+```
+
+### Response
+
+`200 OK` on success:
+
+```json
+{}
+```
+
+`404 Not Found` if the link doesn’t exist or cannot be updated by the authenticated user:
+
+```json
+{
+    "error": "The link does not exist."
+}
+```
+
+`401 Unauthorized` if the request is not authenticated:
+
+```json
+{
+    "error": "The request is not authenticated."
+}
+```
+
+### Example
+
+```console
+$ curl -H "Content-Type: application/json" \
+       -H "Authorization: Bearer <token>" \
+       -X POST \
+       https://app.flus.fr/api/v1/links/<id>/later
+```
---
+++ src/controllers/api/v1/links/Later.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\controllers\api\v1\links;
+
+use App\auth;
+use App\controllers\api\v1\BaseController;
+use App\models;
+use Minz\Request;
+use Minz\Response;
+
+/**
+ * @author  Marien Fressinaud <dev@marienfressinaud.fr>
+ * @license http://www.gnu.org/licenses/agpl-3.0.en.html AGPL
+ */
+class Later extends BaseController
+{
+    /**
+     * @request_param string id
+     *
+     * @response 401
+     *     If the request is not correctly authenticated.
+     * @response 404
+     *     If the link does not exist.
+     * @response 200
+     */
+    public function create(Request $request): Response
+    {
+        $user = $this->requireCurrentUser();
+
+        $link_id = $request->parameters->getString('id', '');
+        $link = models\Link::find($link_id);
+
+        if (!$link || !auth\LinksAccess::canUpdate($user, $link)) {
+            return Response::json(404, [
+                'error' => 'The link does not exist.',
+            ]);
+        }
+
+        $user->markAsReadLater($link);
+
+        return Response::json(200, []);
+    }
+}
