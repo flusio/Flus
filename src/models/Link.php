1e6b2992e4c2cc0b708bbc2a066a570271eaa506
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-06-27 16:06:38 +0200

    new(api): Add endpoints to add and remove links from collections

--- src/models/Link.php
+++ src/models/Link.php
@@ -197,6 +197,22 @@ class Link
     }
 
     /**
+     * Add the link to a collection.
+     */
+    public function addCollection(Collection $collection): void
+    {
+        LinkToCollection::attach([$this->id], [$collection->id]);
+    }
+
+    /**
+     * Remove the link from a collection.
+     */
+    public function removeCollection(Collection $collection): void
+    {
+        LinkToCollection::detach([$this->id], [$collection->id]);
+    }
+
+    /**
      * Return the notes attached to the current link
      *
      * @return Note[]
