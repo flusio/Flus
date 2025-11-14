5fb17289752cc98c555ac1945608b56f41cf5f5c
Marien Fressinaud <marien+dev@fressinaud.fr>
2025-11-14 14:14:27 +0100

    chore: Improve the csrf-loader Stimulus controller

--- src/assets/javascripts/controllers/csrf_controller.js
+++ src/assets/javascripts/controllers/csrf_controller.js
@@ -1,16 +1,19 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static values = {
+        name: String,
+        token: String,
+    }
+
     connect () {
         // We add the csrf token with JavaScript to hopefully block bots which
         // spam registration. Since JS is required to use Flus, it should be
         // transparent for users.
-        const csrfName = document.querySelector('meta[name="csrf-param"]').content;
-        const csrfValue = document.querySelector('meta[name="csrf-token"]').content;
         const csrfInput = document.createElement('input');
         csrfInput.setAttribute('type', 'hidden');
-        csrfInput.setAttribute('name', csrfName);
-        csrfInput.setAttribute('value', csrfValue);
+        csrfInput.setAttribute('name', this.nameValue);
+        csrfInput.setAttribute('value', this.tokenValue);
         this.element.appendChild(csrfInput);
     }
 };
