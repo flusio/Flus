3c0276cb03d47ab4cc92b678b01571ce0a593309
Marien Fressinaud <marien+dev@fressinaud.fr>
2026-05-15 17:44:21 +0200

    chore: Accept callable in Sorter::localeSort

--- src/utils/Sorter.php
+++ src/utils/Sorter.php
@@ -11,18 +11,32 @@ class Sorter
     /**
      * Sort items by the given property, based on the current locale.
      *
+     * $property_or_callable can either be:
+     *
+     * - a string, in which case it must be the name of a string property in the items' class;
+     * - a callable taking an item as a parameter and must return the value to compare.
+     *
      * @template T of object
      *
      * @param T[] $items
+     * @param string|callable(T): string $property_or_callable
      *
      * @return T[]
      */
-    public static function localeSort(array $items, string $property): array
+    public static function localeSort(array $items, string|callable $property_or_callable): array
     {
         $locale = Locale::currentLocale();
         $collator = new \Collator($locale);
-        usort($items, function ($item1, $item2) use ($collator, $property) {
-            $comparison = $collator->compare($item1->$property, $item2->$property);
+        usort($items, function ($item1, $item2) use ($collator, $property_or_callable) {
+            if (is_callable($property_or_callable)) {
+                $value1 = $property_or_callable($item1);
+                $value2 = $property_or_callable($item2);
+            } else {
+                $value1 = $item1->$property_or_callable;
+                $value2 = $item2->$property_or_callable;
+            }
+
+            $comparison = $collator->compare($value1, $value2);
 
             if ($comparison === false) {
                 $comparison = 0;
